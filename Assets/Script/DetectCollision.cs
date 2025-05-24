5561bd97b23055a59f3e8489695963de17fb0df4
saideshiya <ksaideshiya@gmail.com>
2025-05-24 16:04:57 +0530

    Update DetectCollision.cs

--- Assets/Script/DetectCollision.cs
+++ Assets/Script/DetectCollision.cs
@@ -2,8 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DetectCollision : MonoBehaviour
+public class DetectCollider : MonoBehaviour
 {
+    // Start is called before the first frame update
     void Start()
     {
 
@@ -14,6 +15,9 @@ public class DetectCollision : MonoBehaviour
     {
 
     }
-    
-
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(other);
+        Destroy(other.gameObject);
+    }
 }
